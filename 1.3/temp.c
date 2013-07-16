abdfe98079c91850b35c5904f02618ef62059f14
David Wynne <d_wynne@yahoo.com>
2013-07-16 17:43:19 +0100

    Test Travis CI setup

--- 1.3/temp.c
+++ 1.3/temp.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 main()
 {
     printf("%3s %6s\n", "C", "F");
 
-    for (float fahr = 300; fahr >= 0; fahr-=20)
+    for (float fahr = UPPER; fahr >= LOWER; fahr -= STEP)
     {
         printf("%3.0f %6.1f\n", fahr, (5.0/9.0) * (fahr - 32.0));
     }
